b13fae08e8754d5360374dfc8037e68a579a7901
dmitrycvetkov2000 <dimacvetkov2000@mail.ru>
2022-11-13 02:06:21 +0300

    Add base project

---
+++ HW20/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW20
+//
+//  Created by Дмитрий Цветков on 13.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
